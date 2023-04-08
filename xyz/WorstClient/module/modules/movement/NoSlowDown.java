f5dec37088b3abe7f598e3a9e97e442940661b34
ChaoJiXD <chaojiclean@gmail.com>
2023-04-09 07:45:15 +0800

    Velocity/NoSlowDown improvement.

--- xyz/WorstClient/module/modules/movement/NoSlowDown.java
+++ xyz/WorstClient/module/modules/movement/NoSlowDown.java
@@ -7,13 +7,19 @@ import java.awt.Color;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.item.ItemSword;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.C07PacketPlayerDigging;
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
+import net.minecraft.network.play.client.C09PacketHeldItemChange;
+import net.minecraft.network.play.server.S30PacketWindowItems;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import xyz.WorstClient.api.EventHandler;
+import xyz.WorstClient.api.events.world.EventPacketRecieve;
+import xyz.WorstClient.api.events.world.EventPostUpdate;
 import xyz.WorstClient.api.events.world.EventPreUpdate;
+import xyz.WorstClient.api.events.world.EventTarget;
 import xyz.WorstClient.module.Module;
 import xyz.WorstClient.module.ModuleType;
 import xyz.WorstClient.utils.Helper;
@@ -24,6 +30,25 @@ public class NoSlowDown
 extends Module {
     public NoSlowDown() {
         super("NoSlowDown", new String[]{"noslowdown"}, ModuleType.Movement);
+    }
+    @EventTarget
+    public void onPreEvent(EventPreUpdate e){
+        if(shouldBlock()){
+            Minecraft.thePlayer.sendQueue.addToSendQueue(new C09PacketHeldItemChange((Minecraft.thePlayer.inventory.currentItem + 1) % 9));
+            Minecraft.thePlayer.sendQueue.addToSendQueue(new C09PacketHeldItemChange(Minecraft.thePlayer.inventory.currentItem));
+        }
+
+    }
+    @EventTarget
+    public void packet(EventPacketRecieve event) {
+        if (event.getPacket() instanceof S30PacketWindowItems && (mc.thePlayer.isUsingItem() || mc.thePlayer.isBlocking()) && mc.thePlayer.getHeldItem() != null && mc.thePlayer.getHeldItem().getItem() instanceof ItemSword) {
+            event.setCancelled(true);
+        }
+
+    }
+
+    public boolean shouldBlock(){
+        return Minecraft.thePlayer.getHeldItem().getItem() instanceof ItemSword && Minecraft.thePlayer.moving();
     }
 }
 
--- xyz/WorstClient/module/modules/player/AntiVelocity.java
+++ xyz/WorstClient/module/modules/player/AntiVelocity.java
@@ -7,25 +7,40 @@ import net.minecraft.network.play.server.S12PacketEntityVelocity;
 import net.minecraft.network.play.server.S27PacketExplosion;
 import xyz.WorstClient.api.EventHandler;
 import xyz.WorstClient.api.events.world.EventPacketRecieve;
+import xyz.WorstClient.api.value.Mode;
 import xyz.WorstClient.api.value.Numbers;
 import xyz.WorstClient.api.value.Value;
 import xyz.WorstClient.module.Module;
 import xyz.WorstClient.module.ModuleType;
+import xyz.WorstClient.module.modules.movement.Speed;
 
 public class AntiVelocity
 extends Module {
-    private Numbers<Double> percentage = new Numbers<Double>("Percentage", "percentage", 0.0, 0.0, 100.0, 5.0);
+    public static Mode<Enum> mode = new Mode("Mode", "Mode", (Enum[]) kbmode.values(), (Enum) kbmode.hypixel);
+    public Numbers<Double> percent = new Numbers("percent","percent",0.0 , 0.0 ,42.0,42.0);
 
     public AntiVelocity() {
         super("Velocity", new String[]{"antivelocity", "antiknockback", "antikb"}, ModuleType.Player);
-        this.addValues(this.percentage);
         this.setColor(new Color(191, 191, 191).getRGB());
+        this.addValues(mode,percent);
     }
 
     @EventHandler
     private void onPacket(EventPacketRecieve e) {
+        S12PacketEntityVelocity pkt = (S12PacketEntityVelocity)e.getPacket();
         if (e.getPacket() instanceof S12PacketEntityVelocity || e.getPacket() instanceof S27PacketExplosion) {
-            if (this.percentage.getValue().equals(0.0)) {
+            if (mode.getValue() == kbmode.hypixel && mc.thePlayer.onGround) {
+                pkt.motionX = 0;
+                pkt.motionZ = 0;
+            } else if (mode.getValue() == kbmode.zero) {
+                e.setCancelled(true);
+            } else if (mode.getValue() == kbmode.percent){
+                pkt.motionX = (int)(this.percent.getValue() / 100.0);
+                pkt.motionY = (int)(this.percent.getValue() / 100.0);
+                pkt.motionZ = (int)(this.percent.getValue() / 100.0);
+            }
+
+            /*  if (this.percentage.getValue().equals(0.0)) {
                 e.setCancelled(true);
             } else {
                 S12PacketEntityVelocity packet = (S12PacketEntityVelocity)e.getPacket();
@@ -33,7 +48,15 @@ extends Module {
                 packet.motionY = (int)(this.percentage.getValue() / 100.0);
                 packet.motionZ = (int)(this.percentage.getValue() / 100.0);
             }
+        }*/
         }
     }
+        static enum kbmode {
+            zero,
+            percent,
+            hypixel;
+
+        }
+
 }
 
